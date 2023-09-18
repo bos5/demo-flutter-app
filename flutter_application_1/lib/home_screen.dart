317aa8ee251413ac4770cd6b31d36d329a2fbde8
dzung19 <91232320+dzung19@users.noreply.github.com>
2023-09-18 21:11:41 +0700

    create a temp home page

--- flutter_application_1/lib/home_screen.dart
+++ flutter_application_1/lib/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MyHomePage()));
+              },
+              child: const Text('Log out')),
+        ],
+      ),
+    );
   }
 }
